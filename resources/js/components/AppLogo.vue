51968cd04ff6cecd7b0d9cf9010c1378b81c3de9
Talpx1 <simo3401@gmail.com>
2025-05-12 15:37:07 +0200

    feat: init + homepage WIP

---
+++ resources/js/components/AppLogo.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+const { weight = 'medium' } = defineProps<{
+    weight?: 'thin' | 'medium' | 'bold';
+    svgClasses?: string;
+}>();
+</script>
+
+<template>
+    <!-- THIN -->
+    <!-- light -->
+    <svg
+        v-if="weight === 'thin'"
+        class="hidden w-full dark:block"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 96 51"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M1 49.2494C22 49.2494 24.0781 42.1243 34.5 27.7493C49 7.7493 72 -8.7506 95 7.74931C72 7.74931 72 26.2492 72 27.7492C72 29.2492 72 49.2492 95 49.2492"
+            stroke="#FDFFFC"
+            stroke-width="2"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+    <!-- dark -->
+    <svg
+        v-if="weight === 'thin'"
+        class="block w-full dark:hidden"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 96 51"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M1 49.4986C22 49.4986 24.0781 42.3735 34.5 27.9985C49 7.99857 72 -8.50133 95 7.99857C72 7.99857 72 26.4985 72 27.9985C72 29.4985 72 49.4985 95 49.4985"
+            stroke="#040F0F"
+            stroke-width="2"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+
+    <!-- MEDIUM -->
+    <!-- light -->
+    <svg
+        v-if="weight === 'medium'"
+        class="hidden w-full dark:block"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 100 54"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M3 51.2494C24 51.2494 26.0781 44.1243 36.5 29.7493C51 9.7493 74 -6.7506 97 9.74931C74 9.74931 74 28.2492 74 29.7492C74 31.2492 74 51.2492 97 51.2492"
+            stroke="#FDFFFC"
+            stroke-width="5"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+    <!-- dark -->
+    <svg
+        v-if="weight === 'medium'"
+        class="block w-full dark:hidden"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 100 54"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M3 51.4989C24 51.4989 26.0781 44.3738 36.5 29.9988C51 9.99881 74 -6.50109 97 9.99882C74 9.99882 74 28.4987 74 29.9987C74 31.4987 74 51.4987 97 51.4987"
+            stroke="#040F0F"
+            stroke-width="5"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+
+    <!-- BOLD -->
+    <!-- light -->
+    <svg
+        v-if="weight === 'bold'"
+        class="hidden w-full dark:block"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 104 59"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M5 53.2494C26 53.2494 28.0781 46.1243 38.5 31.7493C53 11.7493 76 -4.7506 99 11.7493C76 11.7493 76 30.2492 76 31.7492C76 33.2492 76 53.2492 99 53.2492"
+            stroke="#FDFFFC"
+            stroke-width="10"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+    <!-- dark -->
+    <svg
+        v-if="weight === 'bold'"
+        class="block w-full dark:hidden"
+        :class="svgClasses"
+        preserveAspectRatio="xMidYMid meet"
+        viewBox="0 0 104 59"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M5 53.4989C26 53.4989 28.0781 46.3738 38.5 31.9988C53 11.9988 76 -4.50109 99 11.9988C76 11.9988 76 30.4987 76 31.9987C76 33.4987 76 53.4987 99 53.4987"
+            stroke="#040F0F"
+            stroke-width="10"
+            stroke-linecap="square"
+            stroke-linejoin="bevel"
+        />
+    </svg>
+</template>
---
+++ resources/js/pages/home/components/HomeHeader.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import AppLogo from '@/components/AppLogo.vue';
+</script>
+
+<template>
+    <header class="grid max-h-48 w-full grid-cols-5 text-2xl">
+        <div class="border-dark dark:border-light col-span-2 flex items-center justify-center border-r border-b py-10">
+            <a :href="route('home')">
+                <AppLogo weight="bold" />
+            </a>
+        </div>
+
+        <div class="border-dark dark:border-light flex flex-col border-r">
+            <div class="border-dark dark:border-light relative flex h-1/2 justify-center border-b">
+                <div class="bg-light dark:bg-dark absolute bottom-0 flex translate-y-1/2 items-center gap-2 px-2">
+                    <svg class="cursor-pointer" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_208_98)">
+                            <path
+                                d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
+                                stroke="#FDFFFC"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                            />
+                            <path d="M12 1V3" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M12 21V23" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M4.22 4.21997L5.64 5.63997" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.36 18.36L19.78 19.78" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M1 12H3" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M21 12H23" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M4.22 19.78L5.64 18.36" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M18.36 5.63997L19.78 4.21997" stroke="#FDFFFC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_208_98">
+                                <rect width="24" height="24" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                    <a href="#">ITA/ENG</a>
+                </div>
+            </div>
+            <div class="h-1/2"></div>
+        </div>
+
+        <div class="border-dark dark:border-light flex flex-col border-r">
+            <div class="border-dark dark:border-light relative flex h-1/2 items-center justify-center border-b">
+                <a href="#">ABOUT</a>
+            </div>
+            <div class="flex h-1/2 items-center justify-center">
+                <a href="#">PROGETTI</a>
+            </div>
+        </div>
+
+        <div class="flex flex-col">
+            <div class="border-dark dark:border-light relative flex h-1/2 items-center justify-center border-b">
+                <a href="#">BLOG</a>
+            </div>
+            <div class="bg-dark dark:bg-light text-light dark:text-dark flex h-1/2 items-center justify-center font-extrabold italic">
+                <a href="#">HIRE</a>
+            </div>
+        </div>
+    </header>
+</template>
