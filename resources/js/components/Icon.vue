ecbcca06aba677630a7f9fbbc2a3baaba74e6f4b
Talpx1 <simo3401@gmail.com>
2025-06-10 00:14:25 +0200

    fix: fixed Icon component to allow for size customization

--- resources/js/components/Icon.vue
+++ resources/js/components/Icon.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils';
 import * as icons from 'lucide-vue-next';
 import { computed } from 'vue';
 
 interface Props {
     name: string;
-    class?: string;
+    classes?: string;
     size?: number | string;
     color?: string;
     strokeWidth?: number | string;
@@ -17,8 +16,6 @@ const props = withDefaults(defineProps<Props>(), {
     strokeWidth: 2,
 });
 
-const className = computed(() => cn('h-4 w-4', props.class));
-
 const icon = computed(() => {
     const iconName = props.name.charAt(0).toUpperCase() + props.name.slice(1);
     return (icons as Record<string, any>)[iconName];
@@ -26,5 +23,5 @@ const icon = computed(() => {
 </script>
 
 <template>
-    <component :is="icon" :class="className" :size="size" :stroke-width="strokeWidth" :color="color" />
+    <component :is="icon" :class="classes" :size="size" :stroke-width="strokeWidth" :color="color" />
 </template>
