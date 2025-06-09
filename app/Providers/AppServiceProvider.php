17903422cb4436e4170b883d6c56ff5655b1e4e2
Talpx1 <simo3401@gmail.com>
2025-06-10 00:13:12 +0200

    chore: added some rules to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Sleep;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        Vite::useAggressivePrefetching();
+        Sleep::fake();
+        Date::use(CarbonImmutable::class);
+        Model::shouldBeStrict();
+        Model::unguard();
+        Model::automaticallyEagerLoadRelationships();
+        URL::forceHttps($this->app->environment(['staging', 'production']));
+        DB::prohibitDestructiveCommands($this->app->environment('production'));
+        Http::preventStrayRequests($this->app->runningUnitTests());
     }
 }
