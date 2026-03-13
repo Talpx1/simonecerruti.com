db9b90c479868a5c78df90a68966c1793b5807c4
Talpx1 <simo3401@gmail.com>
2026-03-13 21:57:28 +0000

    fix: mail header logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ 'data:image/svg+xml;base64,' . base64_encode(\Illuminate\Support\Facades\Blade::render('<x-app-logo color="#000" />')) }}"
-                style="max-width: 270px;">
+            <img src="{{ asset('images/logo/logo_strokes_dark.png') }}" style="max-width: 270px;">
             {!! $slot !!}
         </a>
     </td>
